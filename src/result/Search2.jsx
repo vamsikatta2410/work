d362976c04907de738e04e2e158f54aa1a37c622
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-17 09:53:02 +0530

    Removed un-necessary components

--- src/result/Search2.jsx
+++ src/result/Search2.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaMagnifyingGlass } from 'react-icons/fa6'
+import {FaTwitter,FaFacebookF,FaInstagram} from 'react-icons/fa'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 
 import Carousel from 'react-multi-carousel';
@@ -189,7 +190,7 @@ const Search2 = () => {
                 <i className="far fa-angle-double-up"></i>
             </button>
 
-        
+
             <header className="header-bottom__transparent z-index-6 tp-header-height">
                 <div className="header-top__area header-top__space z-index-3 d-none d-md-block tp-header-top-animation">
                     <div className="container">
@@ -269,7 +270,7 @@ const Search2 = () => {
                                 <div className="header-bottom__right d-flex align-items-center justify-content-end">
                                     <div className="header-bottom__action">
                                         <a className="d-none d-md-inline-block search-open-btn" href="javascript:void(0)">
-                                           
+
                                         </a>
                                         <a className="d-none d-lg-inline-block last-child" href="/login">
                                             <svg width="14" height="16" viewBox="0 0 14 16" fill="none"
@@ -549,8 +550,8 @@ const Search2 = () => {
                                         </div>
                                     </div>
                                 </div>
-    
-                                <div className="content" style={{marginBottom:'80px'}}>
+
+                                <div className="content" style={{ marginBottom: '80px' }}>
                                     <div className="services-slider position-relative style-6">
                                         <div className="swiper-container">
                                             <Carousel
@@ -706,7 +707,7 @@ const Search2 = () => {
                         </div>
 
 
-                        <div className="tp-rank__area pb-200">
+                        {/* <div className="tp-rank__area pb-200">
                             <div className="container">
                                 <div className="row">
                                     <div className="col-xl-5 col-lg-5 order-1 order-lg-1">
@@ -818,9 +819,9 @@ const Search2 = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
-                        <div className="tp-price__area tp-price__pl-pr p-relative pt-110 pb-80">
+                        {/* <div className="tp-price__area tp-price__pl-pr p-relative pt-110 pb-80">
                             <div className="container">
                                 <div className="row justify-content-center">
                                     <div className="col-xl-7 wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
@@ -1397,11 +1398,11 @@ const Search2 = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
                     </main>
 
-                    <footer className="pb-50">
+                    {/* <footer className="pb-50">
                         <div className="tp-footer__pl-pr p-relative">
                             <div className="footer-black-bg tp-gsap-bg"></div>
                             <div className="tp-footer__area pt-50">
@@ -1594,6 +1595,147 @@ const Search2 = () => {
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer> */}
+                    <footer className="style-6 border-top brd-gray">
+                        <div className="container">
+                            <div className="row gx-0 justify-content-between">
+                                <div className="col-lg-3 col-sm-6">
+                                    <div className="items">
+                                        <div className="title">
+                                            Bsearch
+                                            {/* - Best for SEO/Marketing */}
+                                        </div>
+                                        <ul>
+                                            <li className="color-666 mb-2">
+                                                <i className="bi bi-house me-1" />
+                                                <small>223 Thatcher Road St, Manhattan</small>
+                                            </li>
+                                            <li className="color-666 mb-2">
+                                                <i className="bi bi-phone me-1" />
+                                                <small>+031 5689 89 98</small>
+                                            </li>
+                                            <li className="color-666 mb-2">
+                                                <i className="bi bi-envelope me-1" />
+                                                <small>contact@itecksolution.co</small>
+                                            </li>
+                                        </ul>
+                                        <div className="socail-icons">
+                                            <a
+                                                href="twiter.html"
+                                                className="icon-35 rounded-circle bg-gray overflow-hidden d-inline-flex align-items-center justify-content-center text-gray me-2"
+                                            >
+                                                <FaTwitter />
+                                            </a>
+                                            <a
+                                                href="facebook.html"
+                                                className="icon-35 rounded-circle bg-gray overflow-hidden d-inline-flex align-items-center justify-content-center text-gray me-2"
+                                            >
+                                                <FaFacebookF />
+                                            </a>
+                                            <a
+                                                href="instagram.html"
+                                                className="icon-35 rounded-circle bg-gray overflow-hidden d-inline-flex align-items-center justify-content-center text-gray"
+                                            >
+                                                <FaInstagram />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-2">
+                                    <div className="items">
+                                        <div className="title">Services</div>
+                                        <ul>
+                                            <li>
+                                                <a href="#">SEO Analysis</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Content Strategy</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Growth Hacking</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Social Media</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Web Design &amp; Develoment</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Google/FB Ads</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="col-lg-2">
+                                    <div className="items">
+                                        <div className="title">Information</div>
+                                        <ul>
+                                            <li>
+                                                <a href="page-about-5.html"> About Bsearch </a>
+                                            </li>
+                                            <li>
+                                                <a href="#"> Investors </a>
+                                            </li>
+                                            <li>
+                                                <a href="page-blog-5.html"> Blog </a>
+                                            </li>
+                                            <li>
+                                                <a href="page-contact-5.html"> Contact </a>
+                                            </li>
+                                            <li>
+                                                <a href="page-about-5.html"> How It Works </a>
+                                            </li>
+                                            <li>
+                                                <a href="#"> Pricing Plan </a>
+                                            </li>
+                                            <li>
+                                                <a href="#"> Covid-19 Update </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-sm-6">
+                                    <div className="items">
+                                        <div className="title">Newsletter</div>
+                                        <small className="color-666">
+                                            Register now to get latest updates on <br /> promotions
+                                            &amp; coupons.{' '}
+                                        </small>
+                                        {/* <form action="https://iteck-html.themescamp.com/contact.php" className="form mt-30" method="post">
+                    <div className="form-group">
+                      <span className="icon">
+                        <i className="bi bi-envelope" />
+                      </span>
+                      <input type="text" placeholder="Enter your email" />
+                      <button> <i className="bi bi-send" /> </button>
+                    </div>
+                  </form>
+                  <small className="color-666 fst-italic mt-20">By subscribing, you accepted our <a href="#" className="fst-normal text-dark">Policy.</a> </small> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="foot">
+                                <div className="row">
+                                    <div className="col-lg-3 col-sm-6">
+                                        {/* <div className="logo">
+                    <img src={logo} alt="" />
+                  </div> */}
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <small className="small d-flex justify-content-center">
+                                            <p>
+                                                © 2023 Copyrights by{' '}
+                                                <span className="fw-bold text-decoration-underline">
+                                                    {' '}
+                                                    Bsearch{' '}
+                                                </span>{' '}
+                                                All Rights Reserved.
+                                            </p>
+                                        </small>
                                     </div>
                                 </div>
                             </div>
