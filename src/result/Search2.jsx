eec5a86b660bc1524d00e5003c07cd8597be5f5e
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-19 16:59:56 +0530

    Added login to home page

--- src/result/Search2.jsx
+++ src/result/Search2.jsx
@@ -194,16 +194,19 @@ const Search2 = () => {
             <header className="header-bottom__transparent z-index-6 tp-header-height">
                 <div style={{ textAlign: 'right', position: 'absolute', top: '80px', right: '120px' }}>
                     <a className="d-none d-lg-inline-block last-child" href="/login" style={{ display: 'flex', alignItems: 'center' }}>
-                        <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 8C8.92882 8 10.4924 6.433 10.4924 4.5C10.4924 2.567 8.92882 1 7 1C5.07118 1 3.50757 2.567 3.50757 4.5C3.50757 6.433 5.07118 8 7 8Z"
-                                stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                            <path d="M13 15C13 12.291 10.3108 10.1 7 10.1C3.68917 10.1 1 12.291 1 15" stroke="currentColor"
-                                strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                        <span style={{ marginLeft: '8px',fontSize:'19px' }}>Log In</span>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 8C8.92882 8 10.4924 6.433 10.4924 4.5C10.4924 2.567 8.92882 1 7 1C5.07118 1 3.50757 2.567 3.50757 4.5C3.50757 6.433 5.07118 8 7 8Z"
+                                    stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M13 15C13 12.291 10.3108 10.1 7 10.1C3.68917 10.1 1 12.291 1 15" stroke="currentColor"
+                                    strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                            <span style={{ marginLeft: '8px', fontSize: '19px' }}>Log In</span>
+                        </div>
                     </a>
                 </div>
+
             </header>
 
 
